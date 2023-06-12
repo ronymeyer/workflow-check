1dcef8981b808182a4763a443ca4f46462a76974
Rony <github.com@spot-light.ch>
2023-06-12 14:59:48 +0200

    Changed from @actions/github to @octokit/rest

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,18 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import { components } from "@octokit/openapi-types";
+import { Octokit } from '@octokit/rest';
 import {
-  getFirst,
   getOptionalInput,
   getOwnerAndRepo,
-  getRepository,
-  logWarning,
-  waitTime
+  getRepository
 } from './utils';
-import { components } from "@octokit/openapi-types";
 
-async function checkWorkflow(token:string, owner:string, repo: string, statusToCheck: components["parameters"]["workflow-run-status"], currentRunId: string, runnerLabel: string): Promise<boolean> {
+async function checkWorkflow(token: string, owner: string, repo: string, statusToCheck: components["parameters"]["workflow-run-status"], currentRunId: string, runnerLabel: string): Promise<boolean> {
   let foundRunningJob = false;
 
-  const octokit = github.getOctokit(token);
-
-  const listWorkflowRunsForRepoResult = await octokit.rest.actions
-  .listWorkflowRunsForRepo({
+  const octo = new Octokit();
+  octo.actions.listWorkflowRunsForRepo()
+  const listWorkflowRunsForRepoResult = await octo.rest.actions.listWorkflowRunsForRepo({
     owner,
     repo,
     status: statusToCheck
@@ -24,7 +20,7 @@ async function checkWorkflow(token:string, owner:string, repo: string, statusToC
 
   core.info(`Received status code: ${listWorkflowRunsForRepoResult.status}, number or results: ${listWorkflowRunsForRepoResult.data.total_count}`);
 
-  let workFlowRunsFiltered = listWorkflowRunsForRepoResult.data.workflow_runs.filter((f)=> f.id != Number(currentRunId));
+  let workFlowRunsFiltered = listWorkflowRunsForRepoResult.data.workflow_runs.filter((f) => f.id != Number(currentRunId));
 
   const workFlowRunsMapped = workFlowRunsFiltered.map((x) => ({
     run_id: x.id,
@@ -32,17 +28,17 @@ async function checkWorkflow(token:string, owner:string, repo: string, statusToC
   }));
 
   for (const workFlowRun of workFlowRunsMapped) {
-    const listJobsForWorkflowRunResult = await octokit.rest.actions
-    .listJobsForWorkflowRun({
-      owner,
-      repo,
-      run_id: workFlowRun.run_id
-    });
+    const listJobsForWorkflowRunResult = await octo.rest.actions
+      .listJobsForWorkflowRun({
+        owner,
+        repo,
+        run_id: workFlowRun.run_id
+      });
 
     core.info(`Received status code: ${listJobsForWorkflowRunResult.status}, number or results: ${listJobsForWorkflowRunResult.data.total_count}`);
 
-    for (const job of listJobsForWorkflowRunResult.data.jobs){
-      if (job.labels.includes(runnerLabel)){
+    for (const job of listJobsForWorkflowRunResult.data.jobs) {
+      if (job.labels.includes(runnerLabel)) {
         foundRunningJob = true;
         break;
       }
@@ -52,16 +48,16 @@ async function checkWorkflow(token:string, owner:string, repo: string, statusToC
   }
 
   // conclusion is null when run is in progress
-    core.info(`foundRunningJob for status ${statusToCheck}: ${foundRunningJob}`);
+  core.info(`foundRunningJob for status ${statusToCheck}: ${foundRunningJob}`);
 
-    return foundRunningJob;
+  return foundRunningJob;
 }
 
 async function run(): Promise<void> {
   try {
-    const token = core.getInput('token', {required: true});
-    const currentRunId = core.getInput('currentRunId', {required: true});
-    const runnerLabel = core.getInput('runnerLabel', {required: true});
+    const token = core.getInput('token', { required: true });
+    const currentRunId = core.getInput('currentRunId', { required: true });
+    const runnerLabel = core.getInput('runnerLabel', { required: true });
     let fullRepo = getOptionalInput('repo');
     if (fullRepo === undefined) {
       fullRepo = getRepository();
@@ -70,12 +66,11 @@ async function run(): Promise<void> {
     core.info(`Checking if there are any running runners with lable ${runnerLabel} which are different to run id ${currentRunId}`);
 
     const [owner, repo] = getOwnerAndRepo(fullRepo);
-    const octokit = github.getOctokit(token);
 
     var foundRunningJob = false
 
     // loop through all statuses to check if we have any other running jobs
-    var statusesToCheck:components["parameters"]["workflow-run-status"][] = ["requested", "queued", "in_progress", "waiting"];
+    var statusesToCheck: components["parameters"]["workflow-run-status"][] = ["requested", "queued", "in_progress", "pending"];
     for (const statusToCheck of statusesToCheck) {
       foundRunningJob = await checkWorkflow(token, owner, repo, statusToCheck, currentRunId, runnerLabel);
       if (foundRunningJob)
